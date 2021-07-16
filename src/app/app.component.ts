d1585464c0ecd282f8c3e662b9f6aac74c1423f5
Angrbeast52 <jonathanmorillocm@hotmail.com>
2021-07-16 10:16:31 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {gql, Apollo} from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rick';
+export class AppComponent implements OnInit {
+
+  characters: any[] = [];
+  loading = true;
+  error: any;
+
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            characters{
+              info{
+                count, 
+                pages, 
+                next,
+                prev
+              }, results{
+                id,
+                name,
+                image,
+                created,
+                status,
+                species,
+                type,
+              }
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.characters = result?.data?.characters.results;
+        this.loading = result.loading;
+        this.error = result.error;
+      });
+  }
+  someFunction(){
+    return this.characters;
+  }
 }
